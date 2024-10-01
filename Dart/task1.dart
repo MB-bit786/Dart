1e97800dbf6cb822d6893b85cdd132edffaf1ac6
MOHAMMED BOOKWALA <114876033+MB-bit786@users.noreply.github.com>
2024-10-01 19:56:46 +0530

    Add files via upload

---
+++ Dart/task1.dart
@@ -0,0 +1,11 @@
+// 1. Display This Information using print
+// • Your Name
+// • Your Birth date
+// • Your Age
+// • Your Address
+void main(List<String> args) {
+ print("Name: mohammed");
+ print("Birth Date: 5th september 2003");
+ print("Age: 21");
+ print("Address: Ahmedabad");
+}
